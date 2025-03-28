efb0c80c1c8c7a2991ad1dd37574b7ebf6d0865f
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-28 15:48:29 +0100

    Course information 7

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -16,7 +16,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  return (<p>Number of exercises {props.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>)
+  return (<p><b>total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>)
 }
 
 const Course = ({course}) => {
@@ -24,7 +24,7 @@ const Course = ({course}) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      {/* <Total parts={course.parts} /> */}
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -48,6 +48,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
