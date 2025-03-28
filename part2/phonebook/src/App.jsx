02e8f1952aad18cecbc6412bff3722be82372cc6
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-28 16:29:02 +0100

    The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,11 +3,16 @@ import { Header2 } from './components/Headers'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -15,8 +20,9 @@ const App = () => {
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already in the phonebook`)
     } else {
-      setPersons(persons.concat({ name: newName }))
+      setPersons(persons.concat({ name: newName, number: newNumber }))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -24,15 +30,14 @@ const App = () => {
     <div>
       <Header2 text='Phonebook' />
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} placeholder='name' onChange={handleNameChange} />
-        </div>
+        <div>name: <input value={newName} placeholder='name' onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} placeholder='number' onChange={handleNumberChange} /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <Header2 text='Numbers' />
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)} 
     </div>
   )
 }
