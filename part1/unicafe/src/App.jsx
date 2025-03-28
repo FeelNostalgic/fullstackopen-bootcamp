9119ac3300f50d68e3775499ef2de6be45a40b3b
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-28 13:55:15 +0100

    unicafe step 5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,26 +1,39 @@
 import { useState } from 'react'
 
-const Header = ({text}) => <h1>{text}</h1>
+const Header = ({ text }) => <h1>{text}</h1>
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const Statistics = ({good, neutral, bad}) => {
-  if(good + neutral + bad === 0) {
-    return(
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+const Statistics = ({ good, neutral, bad }) => {
+  if (good + neutral + bad === 0) {
+    return (
       <div>
         <p>No feedback given</p>
       </div>
     )
   }
 
-  return(
+  return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {((good - bad) / (good + neutral + bad)).toFixed(2)}</p>
-      <p>positive {((good / (good + neutral + bad)) * 100).toFixed(2)}%</p>
+      <table>
+        <tbody>
+          <StatisticsLine text='good' value={good} />
+          <StatisticsLine text='neutral' value={neutral} />
+          <StatisticsLine text='bad' value={bad} />
+          <StatisticsLine text='all' value={good + neutral + bad} />
+          <StatisticsLine text='average' value={((good - bad) / (good + neutral + bad)).toFixed(2)} />
+          <StatisticsLine text='positive' value={((good / (good + neutral + bad)) * 100).toFixed(2) + ' %'} />
+        </tbody>
+      </table>
+
     </div>
   )
 }
@@ -36,13 +49,13 @@ function App() {
 
   return (
     <div>
-    <Header text='give feedback' />
-    <Button onClick={handleGoodClick} text='good' />
-    <Button onClick={handleNeutralClick} text='neutral' />
-    <Button onClick={handleBadClick} text='bad' />
-    <Header text='statistics' />
-    <Statistics good={goodValue} neutral={neutralValue} bad={badValue} />
-  </div>
+      <Header text='give feedback' />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <Header text='statistics' />
+      <Statistics good={goodValue} neutral={neutralValue} bad={badValue} />
+    </div>
   )
 }
 
