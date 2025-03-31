003e5a1bde9ee9ea3ccebe8e32ec93eae0bb2aa3
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-31 11:46:41 +0200

    Preparing -> Data for countries, step 2.5

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,20 +1,11 @@
 import axios from 'axios'
-// const baseUrl = 'http://localhost:3001/countries'
-const apiUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
 
-const getAll = () => {
+const apiUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+const weatherAPIurl = 'https://openweathermap.org/'
+
+const getAllCountries = () => {
     const request = axios.get(`${apiUrl}/all`)
     return request.then(response => response.data)
 }
 
-// const create = newObject => {
-//     const request = axios.post(baseUrl, newObject)
-//     return request.then(response => response.data)
-// }
-
-// const update = (id, newObject) => {
-//     const request = axios.put(`${baseUrl}/${id}`, newObject)
-//     return request.then(response => response.data)
-// }
-
-export default { getAll }
+export default { getAll: getAllCountries }
