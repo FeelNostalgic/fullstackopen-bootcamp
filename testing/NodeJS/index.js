697559f413b860a8d6e6ee9d4d7f92b81d6cf3b1
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-31 16:50:33 +0200

    Integration frontend & backend

--- testing/NodeJS/index.js
+++ testing/NodeJS/index.js
@@ -1,37 +1,7 @@
+require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
 const app = express()
-
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://franciscoaragones2014:${password}@cluster0.exm12.mongodb.net/FullstackBootcamp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-let notes = [
-
-]
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -57,28 +27,13 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then((note) => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
+    .catch(error => response.status(404).end())
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
-
+// POST request to create a new note
 app.post('/api/notes', (request, response) => {
     const body = request.body
     if (!body.content) {
@@ -87,16 +42,24 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
+
+// DELETE request to delete a note
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    })
+    .catch(error => response.status(404).end())
+})
+
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
@@ -104,7 +67,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
