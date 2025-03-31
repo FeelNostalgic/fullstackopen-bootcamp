f439e784124bc084a0638a679881792b562f580c
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-31 15:40:31 +0200

    Phonebook backend step 9

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-
-app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
