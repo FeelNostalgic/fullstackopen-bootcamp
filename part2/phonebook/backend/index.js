67fd7772c4a666f03d4d636d85227843527679f4
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-31 16:24:41 +0200

    Command-line database

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -3,33 +3,34 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.static('dist'))
-app.use(express.json())
-
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
 })
+app.use(express.static('dist'))
+app.use(express.json())
+
+const Note = mongoose.model('Note', noteSchema)
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
@@ -45,7 +46,7 @@ app.get('/info', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
     const person = persons.find(person => person.id === id)
-    if(person) {
+    if (person) {
         response.json(person)
     } else {
         response.status(404).end()
@@ -60,20 +61,20 @@ app.delete('/api/persons/:id', (request, response) => {
 
 const generateId = () => {
     const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => Number(n.id)))
-      : 0
+        ? Math.max(...persons.map(n => Number(n.id)))
+        : 0
     return String(maxId + 1)
-  }
+}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    if(!body.name || !body.number) {
+    if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'name or number missing'
         })
     }
 
-    if(persons.find(person => person.name === body.name)) {
+    if (persons.find(person => person.name === body.name)) {
         return response.status(400).json({
             error: 'name must be unique'
         })
@@ -88,7 +89,7 @@ app.post('/api/persons', (request, response) => {
     persons = persons.concat(person)
 
     response.json(person)
-    
+
 })
 
 
