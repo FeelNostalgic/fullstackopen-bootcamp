2d1c8cfc4c8696f05f64163c10075bf25d7db2d6
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-03-31 17:40:25 +0200

    Phonebook database, step 3-4-5-6

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -22,7 +22,7 @@ app.get('/info', (request, response) => {
     <p>${new Date()}</p>`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
@@ -30,10 +30,7 @@ app.get('/api/persons/:id', (request, response) => {
             response.status(404).end()
         }
     })
-    .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -69,6 +66,18 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
