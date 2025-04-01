be19cae1b4e7bf1d5628eda58437954e7e2e2eae
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-04-01 12:35:34 +0200

    Blog List Tests, step 1

--- blog/backend/app.js
+++ blog/backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('express-async-errors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -20,11 +21,11 @@ mongoose
 
 // app.use(express.static('dist'))
 app.use(express.json())
-// app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 
-// app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
