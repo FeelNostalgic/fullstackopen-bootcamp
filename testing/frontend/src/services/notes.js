1241bdd7f41575fed44e97c0c1a6fabf1ec31ff0
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-04-02 11:41:06 +0200

    Creating new notes

--- testing/frontend/src/services/notes.js
+++ testing/frontend/src/services/notes.js
@@ -1,19 +1,22 @@
 import axios from 'axios'
 const baseUrl = 'api/notes'
 
+let token = null
+
+const setToken = newToken => {
+    token = `bearer ${newToken}`
+}
+
 const getAll = () => {
     const request = axios.get(baseUrl)
-    // const nonExisting = {
-    //     id: 10000,
-    //     content: 'This note is not saved to server',
-    //     important: true
-    // }
-    // return request.then(response => response.data.concat(nonExisting))
     return request.then(response => response.data)
 }
 
 const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
+    const config = {
+        headers: { Authorization: token },
+    }
+    const request = axios.post(baseUrl, newObject, config)
     return request.then(response => response.data)
 }
 
@@ -22,4 +25,4 @@ const update = (id, newObject) => {
     return request.then(response => response.data)
 }
 
-export default { getAll, create, update }
+export default { getAll, create, update, setToken }
