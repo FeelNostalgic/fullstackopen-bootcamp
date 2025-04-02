f3d361c7aaa52ed16e34014a82e506503960a06f
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-04-02 16:17:31 +0200

    Blog List Frontend, step 7

--- blog/frontend/src/components/Blog.jsx
+++ blog/frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import Button from './Button'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const handleLike = () => {
+    console.log('like button clicked')
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <Button onClick={() => toggleVisibility()} text={visible ? 'hide' : 'view'} />
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <div>
+            likes {blog.likes} <Button onClick={() => handleLike()} text='like' />
+          </div>
+          <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
