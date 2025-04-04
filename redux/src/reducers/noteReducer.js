ceb9fc033cb65c1843d78119ac83e3b366c2cf61
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-04-04 16:57:33 +0200

    Asynchronous actions and Redux Thunk

--- redux/src/reducers/noteReducer.js
+++ redux/src/reducers/noteReducer.js
@@ -1,10 +1,11 @@
 import { createSlice, current } from '@reduxjs/toolkit'
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
   initialState: [],
   reducers: {
-    createNote(state, action) {
+    appendNote(state, action) {
       const newNote = action.payload
       state.push(newNote)
     },
@@ -26,5 +27,20 @@ const noteSlice = createSlice({
   },
 })
 
-export const { createNote, toggleImportanceOf, setNotes } = noteSlice.actions
+export const { toggleImportanceOf, setNotes, appendNote } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async (dispatch) => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
+export const createNote = (content) => {
+  return async (dispatch) => {
+    const newNote = await noteService.createNew(content)
+    dispatch(appendNote(newNote))
+  }
+}
+
 export default noteSlice.reducer
