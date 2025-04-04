084844400ba8f2887c40171dddbaab6f2380a73b
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-04-04 17:03:19 +0200

    Anecdotes and the Backend, step 3 & 4

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,7 +1,6 @@
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 import { setNotification, clearNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -10,8 +9,7 @@ const AnecdoteForm = () => {
     e.preventDefault()
     const content = e.target.anecdote.value
     e.target.anecdote.value = ''
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
+    dispatch(createAnecdote(content))
     dispatch(setNotification(`you created '${content}'`))
     setTimeout(() => {
       dispatch(clearNotification())
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,39 @@
-import { createSlice, current } from '@reduxjs/toolkit'  
+import { createSlice, current } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
-    name: 'anecdotes',
-    initialState:[],
-    reducers: {
-      createAnecdote(state, action) {
-        state.push(action.payload)
-      },
-      vote(state, action) {
-        const id = action.payload        
-        const anecdote = state.find(anecdote => anecdote.id === id)
-        anecdote.votes = anecdote.votes + 1
-        console.log(current(state))
-      },
-      setAnecdotes(state, action) {
-        return action.payload
-      }
+  name: 'anecdotes',
+  initialState: [],
+  reducers: {
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    vote(state, action) {
+      const id = action.payload
+      const anecdote = state.find(anecdote => anecdote.id === id)
+      anecdote.votes = anecdote.votes + 1
+      console.log(current(state))
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
-  })
+  }
+})
 
-export const { vote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const { vote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
 export default anecdoteSlice.reducer
