9093f409f830823bc496a1dfd765ab1ecfd2488e
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-04-22 15:37:01 +0200

    Updating a phone number

--- part8/ExampleGrahpQL/frontend-react/src/App.jsx
+++ part8/ExampleGrahpQL/frontend-react/src/App.jsx
@@ -1,14 +1,26 @@
+import {useState} from 'react'
 import { useQuery } from '@apollo/client'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+import PhoneForm from './components/PhoneForm'
+import Notify from './components/Notify'
 
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000
   })
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
 
   if (result.loading) {
     return <div>loading...</div>
@@ -16,10 +28,16 @@ const App = () => {
 
   return (
     <div>
+      <h1>Phonebook</h1>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <h2>Create person</h2>
+      <PersonForm setError={notify} />
+      <h2>Change number</h2>
+      <PhoneForm setError={notify} />
     </div>
   )
 }
 
 export default App
+
