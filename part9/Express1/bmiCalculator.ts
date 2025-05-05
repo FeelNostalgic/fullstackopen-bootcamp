a0e9475d31d806474ebd26e5fd93ff79b118b580
Francisco Aragonés <franciscoaragones2014@gmail.com>
2025-05-05 12:54:08 +0200

    9.5 WebBMI

---
+++ part9/Express1/bmiCalculator.ts
@@ -0,0 +1,11 @@
+type BMI = 'Normal range' | 'Overweight' | 'Underweight'
+
+const calculateBMI = (height: number, weight: number): BMI => {
+  const heightInMeters = height / 100
+  const bmi = weight / (heightInMeters * heightInMeters)
+  if (bmi < 18.5) return 'Underweight'
+  if (bmi < 25) return 'Normal range'
+  return 'Overweight'
+}
+
+export default calculateBMI
--- part9/Express1/index.ts
+++ part9/Express1/index.ts
@@ -1,8 +1,30 @@
 import express from 'express';
+import bmiCalculator from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello World!!!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if(isNaN(height) || isNaN(weight)){
+    res.status(400).send({error: 'malformed parameters'});
+    return;
+  }
+
+  const bmi = bmiCalculator(height, weight);
+
+  let json = {
+    weight: weight,
+    height: height,
+    bmi: bmi
+  }
+
+  res.send(json);
 });
 
 const PORT = 3003;
